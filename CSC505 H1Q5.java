8b0ce02052a77319f2aab2fc7a3d1296bf05a5c5
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-01-20 00:07:56 -0500

    Create CSC505 H1Q5.java

---
+++ CSC505 H1Q5.java
@@ -0,0 +1,13 @@
+public void sol() {
+        double n = 2;
+        double result = 0;
+
+        while(n<1000){
+            if(8*Math.pow(n, 0.8073549221)-6-3*n<0) {
+                System.out.println("The root is around: " + n);
+                return;
+            }
+            n++;
+        }
+        return;
+}
