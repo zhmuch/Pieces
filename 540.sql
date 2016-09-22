855f6b3c1b9a54c72ac67e0ac3b5c7be0abc3bf7
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-09-22 14:36:52 -0400

    Update 540.sql

--- 540.sql
+++ 540.sql
@@ -1,2 +1,16 @@
 -- Find the names and salaries of employees who work in New York.
-SELECT ENAME, SAL FROM EMP WHERE DEPTNO = 30;
+SELECT ENAME, SAL 
+FROM EMP e
+WHERE 
+  e.DEPTNO in (
+    SELECT e.DEPTNO FROM DEPT d WHERE d.LOC = 'NEW YORK'
+  );
+  
+
+-- In which location were new employees hired in the year of 1980.
+SELECT LOC
+FROM DEPT d
+WHERE
+  d.DNAME in (
+    SELECT d.DNAME FROM EMP e WHERE HIREDATE >= '01-JAN-1980' AND HIREDATE <= '31-DEC-1980';
+  );
